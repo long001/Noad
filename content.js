96a8a4b74834e5f9423c5c2dae94f47deff83f48
huanglei <798400626@qq.com>
2019-01-24 14:57:23 +0800

    增加/删除规则时动态隐藏页面广告元素，增加显示当前页面已隐藏广告的数量

--- content.js
+++ content.js
@@ -1,7 +1,31 @@
-getSelectors().then(function (selectorList) {
-  const selectors = selectorList.join(',')
-  const style = document.createElement('style')
-  style.innerHTML = selectors + '{ display:none !important }'
+let adsCount = 0
 
-  document.head.appendChild(style)
+// 页面加载和
+function updateContentStyle () {
+  getSelectors().then(function (selectorList) {
+    // 插入style
+    const styleElem = document.querySelector('#Noad-style') || (function () {
+        const style = document.createElement('style')
+        style.id = 'Noad-style'
+        document.head.appendChild(style)
+        return style
+      }())
+    styleElem.innerHTML = selectorList.join(',') + '{ display:none !important }'
+
+    // 计算屏蔽广告的个数
+    adsCount = selectorList.reduce(function (count, selector) {
+      return count + document.querySelectorAll(selector).length
+    }, 0)
+    sendAdsCount(adsCount)
+  })
+}
+updateContentStyle()
+
+chrome.runtime.onMessage.addListener(function ({ action }) {
+  if (action === 'updateContentStyle') updateContentStyle()
+  else if (action === 'getAdsCount') sendAdsCount(adsCount)
 })
+
+function sendAdsCount (adsCount) {
+  chrome.runtime.sendMessage({ adsCount })
+}
--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,62 @@ const querySelector = document.querySelector.bind(document)
 const ul = querySelector('#ruleList')
 const noDataText = '<li class="rule-list-item">暂无过滤规则 <a class="btn" id="resetBtn">恢复默认</a></li>'
 
-// 页面加载时渲染规则列表
+// 渲染规则列表
+renderList()
+
+// 渲染当前页拦截广告数
+sendMessage({ action: 'getAdsCount' })
+
+// 切换是否对当前网站启用
+querySelector('#checkbox').addEventListener('click', function (e) {
+  const classListAction = e.target.checked ? 'add' : 'remove'
+  document.querySelector('.checkbox-label').classList[classListAction]('checkbox-label-checked')
+})
+
+// 展开收起规则配置列表
+querySelector('#collapseBtn').addEventListener('click', function () {
+  querySelector('#ruleContainer').style.display = (this.open = !this.open) ? 'block' : 'none'
+})
+
+// 点击添加按钮向DOM添加一行li并存储到storage
+querySelector('#addBtn').addEventListener('click', function () {
+  const input = querySelector('#ruleInput')
+  const selector = input.value.trim()
+  if (!selector) return
+
+  if (ul.innerHTML === noDataText) ul.innerHTML = ''
+  ul.appendChild(createListItem(selector))
+  addSelector(selector).then(updateContentStyle) //添加成功后content更新页面样式
+
+  // 清空输入框并且列表滚动到底部
+  input.value = ''
+  ul.scrollTop = ul.scrollHeight
+})
+
+// 点击移除按钮移除该行li并从storage移除该规则
+ul.addEventListener('click', function ({ target }) {
+  if (target.action === 'remove') {
+    const li = target.parentNode
+    ul.removeChild(li)
+    if (ul.children.length === 0) ul.innerHTML = noDataText
+    removeSelector(li.childNodes[0].textContent).then(updateContentStyle) //移除成功后content更新页面样式
+  }
+})
+
+// 点击重置按钮时渲染规则列表并更新content样式
+document.addEventListener('click', function (e) {
+  if (e.target.id=='resetBtn') resetSelectors().then(function () {
+    renderList()
+    updateContentStyle()
+  })
+})
+
+// 跳转到GitHub isssue
+querySelector('#jumpToGitHub').addEventListener('click', function () {
+  chrome.tabs.create({url: 'https://github.com/yeild/Noad/issues'})
+})
+
+
 function renderList () {
   getSelectors().then(function (selectors) {
     if (selectors.length === 0) ul.innerHTML = noDataText
@@ -16,55 +71,17 @@ function renderList () {
     }
   })
 }
-renderList()
 
-// 点击添加按钮向DOM添加一行li并存储到storage
-querySelector('#addBtn').addEventListener('click', function () {
-  const input = querySelector('#ruleInput')
-  const selector = input.value.trim()
-  if (!selector) return
+function updateContentStyle () {
+  sendMessage({ action: 'updateContentStyle' })
+}
 
-  if (ul.innerHTML === noDataText) ul.innerHTML = ''
-  ul.appendChild(createListItem(selector))
-  addSelector(selector)
+function sendMessage (data) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, data)
+  })
+}
 
-  // 清空输入框并且列表滚动到底部
-  input.value = ''
-  ul.scrollTop = ul.scrollHeight
-})
-
-// 点击移除按钮移除该行li并从storage移除该规则
-ul.addEventListener('click', function ({ target }) {
-  if (target.action === 'remove') {
-    confirm('确认删除？')
-    const li = target.parentNode
-    ul.removeChild(li)
-    if (ul.children.length === 0) ul.innerHTML = noDataText
-    removeSelector(li.childNodes[0].textContent)
-  }
-})
-
-querySelector('#checkbox').addEventListener('click', function (e) {
-  const action = e.target.checked ? 'add' : 'remove'
-  document.querySelector('.checkbox-label').classList[action]('checkbox-label-checked')
-})
-
-a = 1
-
-querySelector('#collapseBtn').addEventListener('click', function () {
-  if (a==1) {
-    querySelector('#ruleContainer').style.display = 'block'
-    a = 0
-  } else {
-    a =1
-    querySelector('#ruleContainer').style.display = 'none'
-  }
-})
-
-document.addEventListener('click', function (e) {
-  if (e.target.id=='resetBtn') resetSelectors().then(renderList)
-})
-
-querySelector('#jumpToGitHub').addEventListener('click', function () {
-  chrome.tabs.create({url: 'https://github.com/yeild/Noad/issues'})
+chrome.runtime.onMessage.addListener(function ({ adsCount }) {
+  querySelector('#hiddenAdsCount').innerHTML = adsCount
 })
